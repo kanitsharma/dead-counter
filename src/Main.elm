ccd2e1fbb74fede1310cc5065f1643c9fcc5a5a2
kanitsharma <kanitshrm09@gmail.com>
2018-10-20 17:17:48 -0400

    Updated to count down

--- src/Main.elm
+++ src/Main.elm
@@ -111,7 +111,7 @@ view model =
             [ color (rgb255 0 0 0), alpha 0.7, Font.color (rgb255 255 255 255), width fill, height fill, Font.family [ Font.typeface "marston", Font.sansSerif ], Font.size 95 ]
             [ Element.column
                 [ centerX, centerY ]
-                [ Element.row [] [ text "Red Dead Counter" ]
+                [ Element.row [] [ text "Red Dead Count Down" ]
                 , Element.row [ centerX ] [ text <| getDateString <| getDiffDate model.currentDate model.targetDate ]
                 ]
             ]
@@ -121,7 +121,6 @@ view model =
 -- MAIN
 
 
-<<<<<<< HEAD
 main =
     Browser.element
         { init = init
@@ -129,16 +128,3 @@ main =
         , update = update
         , subscriptions = subscriptions
         }
-=======
-main = 
-    Element.layout [image "https://www.rockstargames.com/reddeadredemption2/rockstar_games/r_d_r2_core/img/screenshots/10-full.jpg"] 
-    columns
-    
-columns = Element.column [ Font.family
-            [ Font.typeface "marston"
-            , Font.sansSerif
-            ], Font.size 45, alpha 0.3, Font.color (rgb255 255 255 255), color (rgb255 0 0 0), width fill, paddingXY 0 20 ]
-    [ 
-        Element.row [ centerX ] [ text "Red Dead Counter" ]
-    ]
->>>>>>> 75a484e7229ee0ea048a435d4460c8d7cdbf6b02
